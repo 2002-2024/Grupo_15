4f5bdca1bdb02cf3683141613d27eb53d92ac52c
2002-2024 <cablleroeduardo@gmail.com>
2024-07-18 23:38:05 -0600

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -5,10 +5,38 @@
  */
 package grupo_15;
 
-/**
- *
- * @author Angie Martinez
- */
-public class Persona {
-    
+
+// Superclase Persona
+class Persona {
+    // Atributos comunes
+    private String nombre;
+    private int edad;
+
+    // Constructor de la superclase
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Método trabajar() de la superclase
+    public void trabajar() {
+        System.out.println("La persona está trabajando.");
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
